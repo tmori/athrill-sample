2b30abed636137e7618bfd36ff3aa6461da59de4
tmori@esm.co.jp <tmori@esm.co.jp>
2019-10-05 11:16:46 +0900

    add sensor device

--- mros/mros_ws/hakoniwa-demo/driver/sensor_device.h
+++ mros/mros_ws/hakoniwa-demo/driver/sensor_device.h
@@ -5,11 +5,13 @@
 #include "vdev.h"
 #include "device_io.h"
 
-#define DEVICE_SENSOR_ADDR_POSE         VDEV_RX_DATA(0)
-#define DEVICE_SENSOR_ADDR_STEARING     VDEV_RX_DATA(1)
-#define DEVICE_SENSOR_ADDR_SPEED        VDEV_RX_DATA(2)
-#define DEVICE_SENSOR_ADDR_OBSTACLE     VDEV_RX_DATA(3)
-#define DEVICE_SENSOR_ADDR_LINE_SENSOR  VDEV_RX_DATA(4)
+#define DEVICE_SENSOR_ADDR_POSE_X               VDEV_RX_DATA(0)
+#define DEVICE_SENSOR_ADDR_POSE_Y               VDEV_RX_DATA(1)
+#define DEVICE_SENSOR_ADDR_STEARING             VDEV_RX_DATA(2)
+#define DEVICE_SENSOR_ADDR_SPEED                VDEV_RX_DATA(3)
+#define DEVICE_SENSOR_ADDR_OBSTACLE_DISTANCE    VDEV_RX_DATA(4)
+#define DEVICE_SENSOR_ADDR_OBSTACLE_ANGLE       VDEV_RX_DATA(5)
+#define DEVICE_SENSOR_ADDR_LINE_SENSOR          VDEV_RX_DATA(6)
 
 static inline void sensor_device_read(uint32 addr, uint32 *data)
 {
