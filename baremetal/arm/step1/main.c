b26cf20452f0f5b9aa4cc35e1923ef9388500d49
tmori@esm.co.jp <tmori@esm.co.jp>
2020-01-03 18:43:11 +0900

    underscore is valid for athrill

--- baremetal/arm/step1/main.c
+++ baremetal/arm/step1/main.c
@@ -4,22 +4,22 @@
 /*
  * RAM BSS
  */
-extern unsigned char _bss_start;
-extern unsigned char _bss_end;
+extern unsigned char bss_start;
+extern unsigned char bss_end;
 
 /*
  * RAM IDATA
  */
-extern unsigned char _data_start;
-extern unsigned char _sdata_start;
+extern unsigned char data_start;
+extern unsigned char sdata_start;
 
 /*
  * ROM IDATA
  */
-extern unsigned char _idata_start;
-extern unsigned char _idata_end;
-extern unsigned char _isdata_start;
-extern unsigned char _isdata_end;
+extern unsigned char idata_start;
+extern unsigned char idata_end;
+extern unsigned char isdata_start;
+extern unsigned char isdata_end;
 
 extern void bss_clear(void);
 extern void data_init(void);
@@ -38,8 +38,8 @@ int main(void)
 
 void bss_clear(void)
 {
-	unsigned char *p = &_bss_start;
-	unsigned char *e = &_bss_end;
+	unsigned char *p = &bss_start;
+	unsigned char *e = &bss_end;
 	for (;p < e; p++) {
 		*p = 0;
 	}
@@ -48,9 +48,9 @@ void bss_clear(void)
 
 void data_init(void)
 {
-	unsigned char *p_rom = &_idata_start;
-	unsigned char *e_rom = &_idata_end;
-	unsigned char *p_ram = &_data_start;
+	unsigned char *p_rom = &idata_start;
+	unsigned char *e_rom = &idata_end;
+	unsigned char *p_ram = &data_start;
 
 	for (;p_rom < e_rom; p_ram++, p_rom++) {
 		*p_ram = *p_rom;
