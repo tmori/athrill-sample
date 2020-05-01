2e4fe09759508c1b30705119fe4c6937ffeecf78
tmori@esm.co.jp <tmori@esm.co.jp>
2020-05-02 08:20:02 +0900

    add asp3 for gr_peach

---
+++ os/asp3_gr_peach_gcc/arch/arm_gcc/common/core_cfg1_out.h
@@ -0,0 +1,10 @@
+/*
+ *		cfg1_out.cのリンクに必要なスタブの定義
+ *
+ *  $Id: core_cfg1_out.h 935 2018-04-07 09:23:40Z ertl-hiro $
+ */
+
+#include <kernel.h>
+
+void sta_ker(void) { }
+STK_T *const	_kernel_istkpt;
